6d2c8b66687fcd4ff9ef6f79691d8ccb3c2a93b6
gonzalopetenar@gmail.com <gonzalpetenar@gmail.com>
2024-03-21 15:33:14 -0600

    new tc

--- tests/shoppingcart.spec.ts
+++ tests/shoppingcart.spec.ts
@@ -1,7 +1,19 @@
 
 import { test, expect } from '@playwright/test';
 
-test('purchase an item', async ({ page }) => {
+test('Validate select item', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/')
+    await page.getByRole('textbox',{name:'Username'}).fill('standard_user')
+    await page.getByRole('textbox',{name:'Password'}).fill('secret_sauce')
+    await page.getByRole('button',{name:'Login'}).click()
+    await page.locator('[name="add-to-cart-sauce-labs-backpack"]').click()
+    await page.locator('[name="remove-sauce-labs-backpack"]').innerText()
+    //await page.locator(".shopping_cart_link").click()
+    //await page.locator('.title').innerText()
+
+});
+
+test('Validate access shopping cart', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/')
     await page.getByRole('textbox',{name:'Username'}).fill('standard_user')
     await page.getByRole('textbox',{name:'Password'}).fill('secret_sauce')
@@ -11,14 +23,5 @@ test('purchase an item', async ({ page }) => {
     await page.locator(".shopping_cart_link").click()
     await page.locator('.title').innerText()
 
-
-
-
     
-    await page.pause()
-
-    
-
-
-
 });
