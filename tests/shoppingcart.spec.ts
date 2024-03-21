796aa0e59228bb12a49f5ceeeb1e56fe8e1dba1d
gonzalopetenar@gmail.com <gonzalpetenar@gmail.com>
2024-03-21 15:26:20 -0600

    mod

--- tests/shoppingcart.spec.ts
+++ tests/shoppingcart.spec.ts
@@ -1,2 +1,24 @@
 
-import {}
+import { test, expect } from '@playwright/test';
+
+test('purchase an item', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/')
+    await page.getByRole('textbox',{name:'Username'}).fill('standard_user')
+    await page.getByRole('textbox',{name:'Password'}).fill('secret_sauce')
+    await page.getByRole('button',{name:'Login'}).click()
+    await page.locator('[name="add-to-cart-sauce-labs-backpack"]').click()
+    await page.locator('[name="remove-sauce-labs-backpack"]').innerText()
+    await page.locator(".shopping_cart_link").click()
+    await page.locator('.title').innerText()
+
+
+
+
+    
+    await page.pause()
+
+    
+
+
+
+});
